a91d2954202efe4bdc9989a2dbcd945b27af9ef4
Gorakh <petter.ohlen@gmail.com>
2024-01-16 03:24:34 +0100

    Flip cost texts to always appear unmirrored

---
+++ VS/MirroredStageVariants/Components/ScaleOnAwakeIfMirrored.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace MirroredStageVariants.Components
+{
+    [DefaultExecutionOrder(-1)]
+    public class ScaleOnAwakeIfMirrored : MonoBehaviour
+    {
+        public Vector3 ScaleMultiplier = Vector3.one;
+
+        void Awake()
+        {
+            if (StageMirrorController.CurrentStageIsMirrored)
+            {
+                transform.localScale = Vector3.Scale(transform.localScale, ScaleMultiplier);
+            }
+        }
+    }
+}
---
+++ VS/MirroredStageVariants/Patches/CostTextMirrorFix.cs
@@ -0,0 +1,25 @@
+﻿using MirroredStageVariants.Components;
+using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace MirroredStageVariants.Patches
+{
+    static class CostTextMirrorFix
+    {
+        [SystemInitializer]
+        static void Init()
+        {
+            GameObject costHologramContent = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Common/VFX/CostHologramContent.prefab").WaitForCompletion();
+            if (costHologramContent)
+            {
+                ScaleOnAwakeIfMirrored scaleOnAwakeIfMirrored = costHologramContent.AddComponent<ScaleOnAwakeIfMirrored>();
+                scaleOnAwakeIfMirrored.ScaleMultiplier = new Vector3(-1f, 1f, 1f);
+            }
+            else
+            {
+                Log.Error("Failed to load cost hologram prefab");
+            }
+        }
+    }
+}
