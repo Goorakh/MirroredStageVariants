afa3f773fefb2e05ebed88f516ce0d009a2f96f5
Gorakh <petter.ohlen@gmail.com>
2024-09-02 20:42:09 +0200

    Fix audio mirror patch not working

--- VS/MirroredStageVariants/Patches/MirrorAudioPatch.cs
+++ VS/MirroredStageVariants/Patches/MirrorAudioPatch.cs
@@ -1,60 +1,110 @@
 ﻿using HarmonyLib;
 using MirroredStageVariants.Components;
 using MirroredStageVariants.Utils;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
 using MonoMod.RuntimeDetour;
+using System;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 namespace MirroredStageVariants.Patches
 {
     static class MirrorAudioPatch
     {
-        delegate Vector3 AkGameObj_GetVectorDelegate(AkGameObj self);
+        delegate int SetObjectPositionDelegate(ulong akObjectId, Vector3 position, Vector3 forward, Vector3 up);
 
-        static Hook AkGameObj_MirrorPosition_Hook;
-        static Hook AkGameObj_MirrorForwardVector_Hook;
-        static Hook AkGameObj_MirrorUpVector_Hook;
+        static GameObject _setObjectPositionSourceObject;
+        static readonly FieldInfo _setObjectPositionSourceObject_FI = AccessTools.DeclaredField(typeof(MirrorAudioPatch), nameof(_setObjectPositionSourceObject));
+
+        static SetObjectPositionDelegate _origSetPosition;
+
+        static readonly List<IDetour> _audioEngineHooks = [];
 
         public static void Apply()
         {
-            AkGameObj_MirrorPosition_Hook = new Hook(SymbolExtensions.GetMethodInfo<AkGameObj>(_ => _.GetPosition()), AkGameObj_GenericTryMirrorPositionResult);
-            AkGameObj_MirrorForwardVector_Hook = new Hook(SymbolExtensions.GetMethodInfo<AkGameObj>(_ => _.GetForward()), AkGameObj_GenericTryMirrorVectorResult);
-            AkGameObj_MirrorUpVector_Hook = new Hook(SymbolExtensions.GetMethodInfo<AkGameObj>(_ => _.GetUpward()), AkGameObj_GenericTryMirrorVectorResult);
+            foreach (MethodInfo soundEngineMethod in typeof(AkSoundEngine).GetMethods(BindingFlags.Public | BindingFlags.Static))
+            {
+                if (!string.Equals(soundEngineMethod.Name, "SetObjectPosition", StringComparison.OrdinalIgnoreCase))
+                    continue;
+
+                ParameterInfo[] parameters = soundEngineMethod.GetParameters();
+                if (parameters.Length < 1)
+                    continue;
+
+                ParameterInfo targetParam = parameters[0];
+                if (targetParam.ParameterType != typeof(GameObject))
+                    continue;
+
+                _audioEngineHooks.Add(new ILHook(soundEngineMethod, il =>
+                {
+                    ILCursor c = new ILCursor(il);
+
+                    c.Emit(OpCodes.Ldarg, targetParam.Position);
+                    c.Emit(OpCodes.Stsfld, _setObjectPositionSourceObject_FI);
+                }));
+            }
+
+            NativeDetour setObjectPositionHook = new NativeDetour(SymbolExtensions.GetMethodInfo(() => AkSoundEnginePINVOKE.CSharp_SetObjectPosition(default, default, default, default)), SymbolExtensions.GetMethodInfo(() => AkSoundEngine_SetObjectPosition(default, default, default, default)));
+            _origSetPosition = setObjectPositionHook.GenerateTrampoline<SetObjectPositionDelegate>();
+
+            _audioEngineHooks.Add(setObjectPositionHook);
         }
 
         public static void Undo()
         {
-            AkGameObj_MirrorPosition_Hook?.Undo();
-            AkGameObj_MirrorForwardVector_Hook?.Undo();
-            AkGameObj_MirrorUpVector_Hook?.Undo();
+            foreach (IDetour audioEngineHook in _audioEngineHooks)
+            {
+                audioEngineHook?.Dispose();
+            }
+
+            _audioEngineHooks.Clear();
+
+            _origSetPosition = null;
         }
 
-        static Matrix4x4 getSoundEmitterMirrorTransformation(AkGameObj self)
+        static bool tryGetSoundEmitterMirrorTransformation(GameObject obj, out Matrix4x4 transformation)
         {
             if (StageMirrorController.CurrentlyIsMirrored)
             {
-                List<AkAudioListener> listeners = self.IsUsingDefaultListeners ? AkAudioListener.DefaultListeners.ListenerList : self.ListenerList;
-                if (listeners.Count == 1)
+                AkGameObj akObj = obj ? obj.GetComponent<AkGameObj>() : null;
+
+                List<AkAudioListener> listeners = akObj && !akObj.IsUsingDefaultListeners ? akObj.ListenerList : AkAudioListener.DefaultListeners.ListenerList;
+                foreach (AkAudioListener listener in listeners)
                 {
-                    AkAudioListener listener = listeners[0];
                     if (listener.GetComponent<MirrorCamera>())
                     {
-                        return listener.transform.GlobalTransformationFromLocal(Matrix4x4.Scale(new Vector3(-1f, 1f, 1f)));
+                        transformation = listener.transform.GlobalTransformationFromLocal(Matrix4x4.Scale(new Vector3(-1f, 1f, 1f)));
+                        return true;
                     }
                 }
             }
 
-            return Matrix4x4.identity;
+            transformation = Matrix4x4.identity;
+            return false;
         }
 
-        static Vector3 AkGameObj_GenericTryMirrorPositionResult(AkGameObj_GetVectorDelegate orig, AkGameObj self)
+        static int AkSoundEngine_SetObjectPosition(ulong akObjectId, Vector3 position, Vector3 forward, Vector3 up)
         {
-            return getSoundEmitterMirrorTransformation(self).MultiplyPoint(orig(self));
-        }
+            // Safety check
+            ulong sourceObjectAkId = AkSoundEngine.GetAkGameObjectID(_setObjectPositionSourceObject);
+            if (sourceObjectAkId == akObjectId)
+            {
+                if (tryGetSoundEmitterMirrorTransformation(_setObjectPositionSourceObject, out Matrix4x4 mirrorTransform))
+                {
+                    position = mirrorTransform.MultiplyPoint(position);
+                    forward = mirrorTransform.MultiplyVector(forward);
+                    up = mirrorTransform.MultiplyVector(up);
+                }
+            }
+            else
+            {
+                Log.Warning($"Object id's do not match! Something has gone wrong with the audio engine hook. sourceObject={_setObjectPositionSourceObject}, sourceObjectId={sourceObjectAkId}, objectId={akObjectId}");
+            }
 
-        static Vector3 AkGameObj_GenericTryMirrorVectorResult(AkGameObj_GetVectorDelegate orig, AkGameObj self)
-        {
-            return getSoundEmitterMirrorTransformation(self).MultiplyVector(orig(self));
+            _setObjectPositionSourceObject = null;
+            return _origSetPosition(akObjectId, position, forward, up);
         }
     }
 }
